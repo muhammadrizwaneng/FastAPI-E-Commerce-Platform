963f14e3e40cc6716f679318f34b1d83fd23c2f5
Muhammad Rizwan Saar <m.rizwan.dev@pk.see.biz>
2025-11-13 12:16:52 +0500

    seeder & ai route comment

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from routes.admin import router as AdminRouter
 from routes.user import router as UserRouter
 from routes.product import router as ProductRouter
 from routes.order import router as OrderRouter
-from routes.aiapi import router as AIRouter
+# from routes.aiapi import router as AIRouter
 from routes.category_router import router as CategoryRouter
 from routes.wishlist_cart import router as WishlistCartRouter
 from fastapi.middleware.cors import CORSMiddleware
-from routes.seed import router as SeedRouter
+# from routes.seed import router as SeedRouter
 
 app = FastAPI()
 
@@ -47,7 +47,7 @@ app.include_router(AdminRouter, tags=["Administrator"], prefix="/admin")
 app.include_router(UserRouter, tags=["User"], prefix="/user")
 app.include_router(ProductRouter, tags=["Product"], prefix="/products")
 app.include_router(OrderRouter, tags=["Orders"], prefix="/order")
-app.include_router(AIRouter, tags=["AI"], prefix="/ai")
+# app.include_router(AIRouter, tags=["AI"], prefix="/ai")
 app.include_router(CategoryRouter, tags=["Category"], prefix="/category")
 app.include_router(WishlistCartRouter, tags=["Wishlist & Cart"], prefix="/wishlist_cart")
-app.include_router(SeedRouter, tags=["Seed"], prefix="/seed")
+# app.include_router(SeedRouter, tags=["Seed"], prefix="/seed")
